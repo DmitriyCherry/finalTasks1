1c0ab9d1ce7321cd1fcaf25db94b432f175f3f4a
Dmitriy <dmitr.wischnewski@mail.ru>
2023-03-01 16:25:58 +0300

    First task is end

---
+++ firstTask/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+
+
+
+int PrintOrderNumbers(int value)
+{
+    string emptyLine = "\"";
+    if (value == 1) Console.Write(value + emptyLine);
+    else Console.Write($"{value}, ");
+
+    if (value == 1) return value;
+    return PrintOrderNumbers(value - 1);
+}
+
+string empty = "\"";        
+Console.Write("Введите число: ");
+int count = int.Parse(Console.ReadLine()!);
+Console.Write($"N = {count} -> {empty}");
+int result = PrintOrderNumbers(count);
