bf04d09b2314b5e1e4b28881c8abce55fa60cd86
Dmitriy <dmitr.wischnewski@mail.ru>
2023-03-02 16:47:04 +0300

    Second task is end

---
+++ secondTask/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+
+int SumInterval(int beginInterval, int endInterval)
+{
+    int sum = 0;
+    while (beginInterval <= endInterval)
+    {
+        sum += beginInterval;
+        beginInterval++;
+    }
+    return sum;
+}
+
+void main()
+{
+    Console.Write("Введите начало интервала: ");
+    int countM = int.Parse(Console.ReadLine()!);
+    Console.Write("Введите конец интервала: ");
+    int countN = int.Parse(Console.ReadLine()!); ;
+    int result = SumInterval(countM, countN);
+    Console.Write($"M = {countM} ; N = {countN} -> {result}");
+}
+
+main();
